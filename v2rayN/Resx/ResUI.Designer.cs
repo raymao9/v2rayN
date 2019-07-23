544a43c4e2c32d021648c8fc6e0ecf6843fa69a3
work@3mao.icu <work@3mao.icu>
2019-07-23 18:25:09 +0800

    0723

--- v2rayN/Resx/ResUI.Designer.cs
+++ v2rayN/Resx/ResUI.Designer.cs
@@ -520,7 +520,7 @@ namespace v2rayN.Resx {
         }
         
         /// <summary>
-        ///   查找类似 未安裝 v2ray-core，請利用 [線上更新] 功能自動下載 的本地化字符串。
+        ///   查找类似 未安裝 v2ray-Core，請利用 [線上更新] 功能自動下載 的本地化字符串。
         /// </summary>
         internal static string NotFoundCore {
             get {
