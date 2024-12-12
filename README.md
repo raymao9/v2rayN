a60f20e9c7479e29e7ae7c10154aaf551d068734
raymao9 <28225813+raymao9@users.noreply.github.com>
2024-12-12 15:18:58 +0800

    v6.45
    master 分支目前與 zh_TW 保持一致

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# V2rayN for Windows
-這是一個基於 [2dust/v2rayN](https://github.com/2dust/v2rayN) 的微調定制版本，需要原版的請前往原開發者主頁獲取。
-本軟體需要搭配 v2ray-Core 才可使用，v3.29.1 版本已内置 v2ray-core 4.34 版本，用戶安裝後直接可以運行。
+# v2rayN
+## 介紹
+1. 基於 [2dust/v2rayN](https://github.com/2dust/v2rayN) 專案的原版代碼
+2. 將繁體中文介面語言優化為台灣習慣用語
+3. 移除軟體內的推廣內容及自動更新相關功能
+
+## 分支說明
+1. zh_TW 分支為繁體中文版本
+2. zh_CN 分支為簡體中文版本
---
+++ v2rayN/v2rayN/ViewModels/AddServerViewModel.cs
@@ -0,0 +1,152 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using Splat;
+using System.Reactive;
+using System.Windows;
+using v2rayN.Handler;
+using v2rayN.Models;
+using v2rayN.Resx;
+
+namespace v2rayN.ViewModels
+{
+    public class AddServerViewModel : ReactiveObject
+    {
+        private static Config _config;
+        private NoticeHandler? _noticeHandler;
+        private Window _view;
+
+        [Reactive]
+        public ProfileItem SelectedSource { get; set; }
+
+        public ReactiveCommand<Unit, Unit> SaveCmd { get; }
+
+        public AddServerViewModel(ProfileItem profileItem, Window view)
+        {
+            _config = LazyConfig.Instance.GetConfig();
+            _noticeHandler = Locator.Current.GetService<NoticeHandler>();
+            _view = view;
+
+            if (profileItem.id.IsNullOrEmpty())
+            {
+                profileItem.network = Global.DefaultNetwork;
+                profileItem.headerType = Global.None;
+                profileItem.requestHost = "";
+                profileItem.streamSecurity = "";
+                SelectedSource = profileItem;
+            }
+            else
+            {
+                SelectedSource = JsonUtils.DeepCopy(profileItem);
+            }
+
+            SaveCmd = ReactiveCommand.Create(() =>
+            {
+                SaveServer();
+            });
+
+            Utils.SetDarkBorder(view, _config.uiItem.followSystemTheme ? !Utils.IsLightTheme() : _config.uiItem.colorModeDark);
+        }
+
+        private void SaveServer()
+        {
+            if (Utils.IsNullOrEmpty(SelectedSource.remarks))
+            {
+                _noticeHandler?.Enqueue(ResUI.PleaseFillRemarks);
+                return;
+            }
+
+            if (Utils.IsNullOrEmpty(SelectedSource.address))
+            {
+                _noticeHandler?.Enqueue(ResUI.FillServerAddress);
+                return;
+            }
+            var port = SelectedSource.port.ToString();
+            if (Utils.IsNullOrEmpty(port) || !Utils.IsNumeric(port)
+                || SelectedSource.port <= 0 || SelectedSource.port >= Global.MaxPort)
+            {
+                _noticeHandler?.Enqueue(ResUI.FillCorrectServerPort);
+                return;
+            }
+            if (SelectedSource.configType == EConfigType.Shadowsocks)
+            {
+                if (Utils.IsNullOrEmpty(SelectedSource.id))
+                {
+                    _noticeHandler?.Enqueue(ResUI.FillPassword);
+                    return;
+                }
+                if (Utils.IsNullOrEmpty(SelectedSource.security))
+                {
+                    _noticeHandler?.Enqueue(ResUI.PleaseSelectEncryption);
+                    return;
+                }
+            }
+            if (SelectedSource.configType != EConfigType.Socks
+                && SelectedSource.configType != EConfigType.Http)
+            {
+                if (Utils.IsNullOrEmpty(SelectedSource.id))
+                {
+                    _noticeHandler?.Enqueue(ResUI.FillUUID);
+                    return;
+                }
+            }
+
+            var item = LazyConfig.Instance.GetProfileItem(SelectedSource.indexId);
+            if (item is null)
+            {
+                item = SelectedSource;
+            }
+            else
+            {
+                item.coreType = SelectedSource.coreType;
+                item.remarks = SelectedSource.remarks;
+                item.address = SelectedSource.address;
+                item.port = SelectedSource.port;
+
+                item.id = SelectedSource.id;
+                item.alterId = SelectedSource.alterId;
+                item.security = SelectedSource.security;
+                item.flow = SelectedSource.flow;
+
+                item.network = SelectedSource.network;
+                item.headerType = SelectedSource.headerType;
+                item.requestHost = SelectedSource.requestHost;
+                item.path = SelectedSource.path;
+
+                item.streamSecurity = SelectedSource.streamSecurity;
+                item.sni = SelectedSource.sni;
+                item.allowInsecure = SelectedSource.allowInsecure;
+                item.fingerprint = SelectedSource.fingerprint;
+                item.alpn = SelectedSource.alpn;
+
+                item.publicKey = SelectedSource.publicKey;
+                item.shortId = SelectedSource.shortId;
+                item.spiderX = SelectedSource.spiderX;
+            }
+
+            var ret = item.configType switch
+            {
+                EConfigType.VMess => ConfigHandler.AddServer(_config, item),
+                EConfigType.Shadowsocks => ConfigHandler.AddShadowsocksServer(_config, item),
+                EConfigType.Socks => ConfigHandler.AddSocksServer(_config, item),
+                EConfigType.Http => ConfigHandler.AddHttpServer(_config, item),
+                EConfigType.Trojan => ConfigHandler.AddTrojanServer(_config, item),
+                EConfigType.VLESS => ConfigHandler.AddVlessServer(_config, item),
+                EConfigType.Hysteria2 => ConfigHandler.AddHysteria2Server(_config, item),
+                EConfigType.Tuic => ConfigHandler.AddTuicServer(_config, item),
+                EConfigType.Wireguard => ConfigHandler.AddWireguardServer(_config, item),
+                _ => -1,
+            };
+
+            if (ret == 0)
+            {
+                _noticeHandler?.Enqueue(ResUI.OperationSuccess);
+                _view.DialogResult = true;
+                //_view?.Close();
+            }
+            else
+            {
+                _noticeHandler?.Enqueue(ResUI.OperationFailed);
+            }
+        }
+    }
+}
